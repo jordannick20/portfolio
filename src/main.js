808455be367d34cbd5ee16ad22faea434f2fb7b6
jordan <nicholsonjordan48@gmail.com>
2026-04-30 15:02:11 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../sass/styles.scss";
+import "spin.js/spin.css";
+import { Spinner } from "spin.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const overlay = document.querySelector(".g-loading-overlay");
+    const page = document.querySelector("#p-page");
+
+    let spinner = new Spinner({
+        color: "#ffffff",
+        lines: 10
+    }).spin(overlay);
+
+    function hideLoading() {
+        spinner.stop();
+        overlay.style.display = "none";
+        page.classList.add("loaded");
+    }
+
+    function showLoading() {
+        overlay.style.display = "block";
+
+        spinner = new Spinner({
+            color: "#ffffff",
+            lines: 10
+        }).spin(overlay);
+
+        page.classList.remove("loaded");
+    }
+
+    window.addEventListener("load", hideLoading);
+
+    document.querySelectorAll("nav a").forEach(link => {
+        link.addEventListener("click", showLoading);
+    });
+
+});
